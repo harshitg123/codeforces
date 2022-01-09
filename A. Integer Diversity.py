69070167efbb642e0205fea74fa0594eab1b8699
harshit gupta <harshit.28.sps@gmail.com>
2022-01-09 12:07:45 +0530

    Div 3 A 800

--- A. Integer Diversity.py
+++ A. Integer Diversity.py
@@ -1,19 +1,18 @@
-# A. Linear Keyboard: https://codeforces.com/problemset/problem/1607/A
+# A. Integer Diversity: https://codeforces.com/problemset/problem/1616/A
 
 for i in range(int(input())):
     count = 0
     unique = []
     n = int(input())
     lis = [int(j) for j in input().split()]
-    
+
     for k in range(0, n):
-        
+
         if lis[k] not in unique:
             unique.append(lis[k])
-            
+
         else:
             if lis[k]*-1 not in unique:
                 unique.append(lis[k]*-1)
 
     print(len(unique))
-    
